e5292ce74b9d737bfceec3e86f24133a28cbb86d
GeorgeOrfa015 <orfanidesg15.main@gmail.com>
2024-04-27 01:41:27 +0300

    Commit 117 General - Files used for testing are now found in ./.indev/ Math - Updated the layout to the new one with minor changes - Added Arithmetic Progression Group with one module, a_n calculator. More related to APs will be coming soon. - Removed Triangular number calculator which will be replaced in the future by an AP sum calculator.

--- math/index.js
+++ math/index.js
@@ -3,12 +3,11 @@ function calc1() {
     let output = document.querySelector("#calc1out"); 
     let factors = "";
     if (input.value!=0||!isNaN(input.value)){
-        for (i=1;i<=Math.floor(input.value/2+1);i++) {
+        for (i=1;i<=input.value;i++) {
             if (input.value%i==0) {
                 factors+=i+", ";
             };
         };
-        factors = factors+=input.value+", ";
         factors = factors.slice(0, -2); 
     }else{
         factors = "undefined";
@@ -16,23 +15,55 @@ function calc1() {
     output.innerHTML=factors;
 }
 function calc2() {
-    let input = document.querySelector("#calc2in").value;
-    let output = document.querySelector("#calc2out");
-    let sum = 0;
-    let desc = "";
-    for (i=1;i<=input;i++) {
-        sum+=i
-        desc+=i+" + "
-    }
-    desc = desc.slice(0, -2);
-
-    if (desc=="") {
-        desc = 0
-    }
-    if (input=="") {
-        output.innerHTML="";
+    let a1 = Number(document.getElementById("calc2in1").value);
+    let d = Number(document.getElementById("calc2in2").value);
+    let n = Number(document.getElementById("calc2in3").value);
+    let out1 = document.getElementById("calc2out1");
+    let out2 = document.getElementById("calc2out2");
+    let an = a1 + (n-1) * d;
+    let a2 = a1 + d;
+    let a3 = a2 + d;
+    let a4 = a3 + d;
+    if (a1==""&&d=="") {
+        out1.innerHTML = "";
+        if (n!="") {
+            out1.innerHTML = "ERR: Please define a progression."
+        };
+        out2.innerHTML = "";
+    }else if (a1==""||d==""||isNaN(a1)||isNaN(d)) {
+        out1.innerHTML = "ERR: Please define the progression correctly.";
+        out2.innerHTML = "";
     }else{
-        output.innerHTML=sum+" = "+desc;
+        if (isNaN(n)) {
+            out1.innerHTML = 'ERR: Please input a number into the "n" field.';
+            out2.innerHTML = "";
+        }else if(n=="") {
+            out1.innerHTML = `${a1}, ${a2}, ${a3}, ${a4} ... a<sub>n</sub>`;
+            out2.innerHTML = "a<sub>n</sub>";
+        }else{
+            if (n>=6) {
+                out1.innerHTML = `${a1}, ${a2}, ${a3}, ${a4} ... ${an}`;
+                out2.innerHTML = `a<sub>${n}</sub> = ${an}`;
+            }else if (n==5) {
+                out1.innerHTML = `${a1}, ${a2}, ${a3}, ${a4}, ${an}`;
+                out2.innerHTML = `a<sub>${n}</sub> = ${an}`;
+            }else if (n==4) {
+                out1.innerHTML = `${a1}, ${a2}, ${a3}, ${an}`;
+                out2.innerHTML = `a<sub>${n}</sub> = ${an}`;
+            }else if (n==3) {
+                out1.innerHTML = `${a1}, ${a2}, ${an}`;
+                out2.innerHTML = `a<sub>${n}</sub> = ${an}`;
+            }else if (n==2) {
+                out1.innerHTML = `${a1}, ${an}`;
+                out2.innerHTML = `a<sub>${n}</sub> = ${an}`;
+            }else if (n==1) {
+                out1.innerHTML = `${an}`;
+                out2.innerHTML = `a<sub>${n}</sub> = ${an}`;
+            }else if (n==0) {
+                out1.innerHTML = "ERR: a<sub>n</sub> with n less or equal to 0 is not defined."
+                out2.innerHTML = "";
+            }
+        }
     }
 }
 function calc3() {
@@ -94,5 +125,4 @@ function calc4() {
             output.innerHTML="Error: Input contains text." //error text in array
         }
     }
-    
 }
